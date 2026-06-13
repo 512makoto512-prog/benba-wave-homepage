6e9bb2cf10d856c4269333d2a0c75abcaf828118
512makoto512-prog <512makoto512@gmail.com>
2026-06-13 10:13:47 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,4 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
 
-const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      { protocol: "https", hostname: "images.unsplash.com" },
-      { protocol: "https", hostname: "picsum.photos" },
-    ],
-  },
-};
-
-export default nextConfig;
+export default nextConfig
